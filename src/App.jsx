16b46e4e867a1d1cb324423b56aabc7e8a60acb9
sajolkk <sajolkar56@gmail.com>
2024-07-06 00:24:56 +0600

    react router project initialize

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center font-bold text-3xl">React Router</h1>
+    </>
+  );
+}
+
+export default App;
